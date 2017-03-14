ebf36440d5898d82c1ffc7c96e1901e89cf59893
Cyril Margorin <cyril@imperx.com>
2017-03-14 20:02:29 +0300

    Add config.h file support

--- ndhcpd.cc
+++ ndhcpd.cc
@@ -1,6 +1,7 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <ndhcpd.hpp>
+#include "config.h"
 #include "ndhcpd_p.hpp"
 
 #include <arpa/inet.h>
--- ndhcpd_p.cc
+++ ndhcpd_p.cc
@@ -1,6 +1,7 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "ndhcpd_p.hpp"
+#include "config.h"
 
 #include <netdb.h>
 #include <net/if.h>
